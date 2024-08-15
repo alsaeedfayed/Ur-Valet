1cb3ecbe0d00975eff516870cf3ff75114ec0fb6
alsaeedfayed <alsaeedfayed@gmail.com>
2024-08-15 05:54:39 -0800

    feat: :fire: add click listner to menue items
    close mene items on click li and go to the item section

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ function switchLanguage(lang) {
 
 document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.querySelector(".header__menu-toggle");
+  const listItems = document.querySelectorAll("#myList li");
   const navBar = document.querySelector(".header__nav-bar");
 
   menuToggle.addEventListener("click", () => {
@@ -15,6 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
     menuToggle.classList.toggle("active");
   });
 
+  listItems.forEach(function (item) {
+    item.addEventListener("click", handleClickListItem);
+  });
+
+  function handleClickListItem() {
+    navBar.classList.remove("active");
+    menuToggle.classList.remove("active");
+  }
   const currentYear = new Date().getFullYear();
   document.getElementById("year").textContent = currentYear;
 });
